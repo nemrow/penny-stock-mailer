23a6ba8597617016e9d87dbd170e829e901360ec
Nemrow <nemrowj@gmail.com>
2015-12-29 09:36:11 -0800

    redo order

--- app/workers/master_continuous_worker.rb
+++ app/workers/master_continuous_worker.rb
@@ -3,7 +3,10 @@ class MasterContinuousWorker
 
   def perform
     UpdateAllStocksWorker.perform_async
-    StockScannerWorker.perform_async
     ScanForSellableStocksWorker.perform_async
+
+    # this one will be separate and will change because it
+    # needs to be run with different vars per user settings
+    StockScannerWorker.perform_async
   end
 end
